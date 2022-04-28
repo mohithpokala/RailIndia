629299863576259d8a715e7c09a1325f5b1335bc
preetham02 <preetham@cse.iitb.ac.in>
2022-04-28 14:28:35 +0530

    added tokens

--- new_server/Controllers/find_user.controller.js
+++ new_server/Controllers/find_user.controller.js
@@ -1,9 +1,13 @@
+const { use } = require("bcrypt/promises");
 const {get_user} = require("../Models/find_user.models.js");
 
 const find_user = async (req,res) => {
+ 
+    const { user_name, password } = req.body;
+
     const data = await get_user(
-        req.params.user_name,
-        req.params.password
+        user_name,
+        password
     );
     return res.status(200).json(data);
 }
--- new_server/Models/database.js
+++ new_server/Models/database.js
@@ -2,9 +2,9 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Mohith@2002",  host: "localhost",
+  password: "1729",  host: "localhost",
   port: 5432,
-  database: "Railway"
+  database: "railindia"
 });
 
 module.exports = pool;
