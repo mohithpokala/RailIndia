9319692c04502dd315fed6eccfa56df96997f6ff
mohithpokala <pokalamohith@gmail.com>
2022-04-28 23:09:24 +0530

    added stats page

--- client/src/pages/Logistics.js
+++ client/src/pages/Logistics.js
@@ -94,20 +94,103 @@ const Schedules2 = (props) => {
 
                         <table>
                             <tr>
-                                <td><b>S.No</b></td>
-                                <td><b>Station Name</b></td>
                                 <td><b>Station Code</b></td>
-                                <td><b></b></td>
-                                <td><b>Distance from Source</b></td>
+                                <td><b>Station Name</b></td>
+                                <td><b>Inflow</b></td>
                             </tr>
                             {
-                                scheduled.map((row) => (
+                                top5inflow.map((row) => (
                                     <tr>
-                                        <td><b>{row.path_id}</b></td>
+                                        <td><b>{row.station_id}</b></td>
                                         <td><a href={"/station_schedule/"+row.station_id}>{row.station_name}</a></td>
-                                        <td>{row.expected_arrival_time}</td>
-                                        <td>{row.expected_departure_time}</td>
-                                        <td>{row.distance_from_source}</td>
+                                        <td>{row.x}</td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
+                        <br></br><br></br>
+
+                        <h4>
+                            TOP 5 stations with highest outflow
+                        </h4>
+
+                        <table>
+                            <tr>
+                                <td><b>Station Code</b></td>
+                                <td><b>Station Name</b></td>
+                                <td><b>Outflow</b></td>
+                            </tr>
+                            {
+                                top5outflow.map((row) => (
+                                    <tr>
+                                        <td><b>{row.station_id}</b></td>
+                                        <td><a href={"/station_schedule/"+row.station_id}>{row.station_name}</a></td>
+                                        <td>{row.x}</td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
+
+                        <br></br><br></br>
+
+                        <h4>
+                            Zone wise train count
+                        </h4>
+
+                        <table>
+                            <tr>
+                                <td><b>Zone </b></td>
+                                <td><b>Count</b></td>
+                            </tr>
+                            {
+                                zonestat1.map((row) => (
+                                    <tr>
+                                        <td><b>{row.zone}</b></td>
+                                        <td>{row.count}</td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
+
+                        <h4>
+                            Zone wise station count
+                        </h4>
+
+                        <table>
+                            <tr>
+                                <td><b>Zone </b></td>
+                                <td><b>Count</b></td>
+                            </tr>
+                            {
+                                zonestat2.map((row) => (
+                                    <tr>
+                                        <td><b>{row.zone}</b></td>
+                                        <td>{row.count}</td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
+
+                        <h4>
+                            Longest trains
+                        </h4>
+
+                        <table>
+                            <tr>
+                                <td><b>Source</b></td>
+                                <td><b>Destination</b></td>
+                                <td><b>Distance</b></td>
+                                <td><b>Train Number </b></td>
+                                <td><b>Train Name</b></td>
+                            </tr>
+                            {
+                                zonestat1.map((row) => (
+                                    <tr>stat1,stat2,dist,train_no,train_name
+                                        <td><b>{row.stat1}</b></td>
+                                        <td>{row.stat2}</td>
+                                        <td><b>{row.dist}</b></td>
+                                        <td>{row.train_no}</td>
+                                        <td><b>{row.train_name}</b></td>
                                     </tr>
                                 ))
                             }
